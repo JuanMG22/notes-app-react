a68f149917ef3014e9a496b359a0db87e321f71b
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-03-22 19:42:05 -0300

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <header className='h-[3rem] fixed top-0 z-50 w-full bg-gray-900'>
       <nav className='flex justify-evenly'>
-        <li className='cursor-pointer lg:w-1/6 w-2/4 flex items-center justify-center truncate uppercase select-none font-semibold rounded-full py-2 h-full hover:bg-indigo-600  hover:transform hover:transition-transform text-xl list-none transition-all transition-200'>Notes App</li>
+        <li className='cursor-pointer lg:w-1/6 w-2/4 flex items-center justify-center truncate uppercase select-none font-semibold rounded py-2 h-full hover:bg-indigo-600  hover:transform hover:transition-transform text-xl list-none transition-all transition-200'>Notes App</li>
 
       </nav>
     </header>
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -14,6 +14,11 @@ const NotesList = ({ loading, notes, setNotes, setLoading }) => {
               className='container mb-2 flex mx-auto w-full items-center justify-center'
             >
               <ul className='flex flex-col p-4'>
+                {notes.length === 0 && (
+                  <h3 className='mt-5 text-xl text-gray-100'>
+                    No hay ninguna nota aún
+                  </h3>
+                )}
                 {notes.map((note) =>
                   <Note
                     key={note.id}
